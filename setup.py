6e0d7161cbf9ea7040dfc42b0e531a81f6bb8fca
G.Allegri <giohappy@gmail.com>
2025-12-11 13:04:14 +0100

    Updated readme

--- setup.py
+++ setup.py
@@ -1,71 +1,12 @@
 from setuptools import find_packages, setup
-
-VERSION = "6.0.0.2"
-LONG_DESCRIPTION = """
-.. image:: http://pinaxproject.com/pinax-design/patches/pinax-notifications.svg
-    :target: https://pypi.python.org/pypi/pinax-notifications/
-
-===================
-Pinax Notifications
-===================
-
-.. image:: https://img.shields.io/pypi/v/pinax-notifications.svg
-    :target: https://pypi.python.org/pypi/pinax-notifications/
-
-\
-
-.. image:: https://img.shields.io/circleci/project/github/pinax/pinax-notifications.svg
-    :target: https://circleci.com/gh/pinax/pinax-notifications
-.. image:: https://img.shields.io/codecov/c/github/pinax/pinax-notifications.svg
-    :target: https://codecov.io/gh/pinax/pinax-notifications
-.. image:: https://img.shields.io/github/contributors/pinax/pinax-notifications.svg
-    :target: https://github.com/pinax/pinax-notifications/graphs/contributors
-.. image:: https://img.shields.io/github/issues-pr/pinax/pinax-notifications.svg
-    :target: https://github.com/pinax/pinax-notifications/pulls
-.. image:: https://img.shields.io/github/issues-pr-closed/pinax/pinax-notifications.svg
-    :target: https://github.com/pinax/pinax-notifications/pulls?q=is%3Apr+is%3Aclosed
-
-\
-
-.. image:: http://slack.pinaxproject.com/badge.svg
-    :target: http://slack.pinaxproject.com/
-.. image:: https://img.shields.io/badge/license-MIT-blue.svg
-    :target: https://opensource.org/licenses/MIT
-
-\
-
-``pinax-notifications`` is a user notification management app for the Django web framework.
-
-``pinax-notifications`` notifies users when certain events have occurred and allows
-configurable options for how those notifications are received.
-
-Features
---------
-
-* Submission of notification messages by other apps
-* Notification messages on signing in
-* Notification messages via email (configurable by user)
-* Ability to supply your own backend notification channels
-* Ability to scope notifications at the site level
-
-Supported Django and Python Versions
-------------------------------------
-
-+-----------------+-----+-----+-----+
-| Django / Python | 3.8 | 3.9 | 3.10 |
-+=================+=====+=====+=====+
-|  4.2            |  *  |  *  |  *  |
-+-----------------+-----+-----+-----+
-"""
-
 setup(
     author="Pinax Team",
     author_email="team@pinaxproject.com",
-    description="User notification management for the Django web framework",
-    long_description_content_type="text/markdown",
     name="geonode-pinax-notifications",
-    long_description=LONG_DESCRIPTION,
-    version=VERSION,
+    version="6.0.0.2",
+    description="User notification management for the Django web framework",
+    long_description="README.md",
+    long_description_content_type="text/markdown",
     url="http://github.com/pinax/pinax-notifications/",
     license="MIT",
     packages=find_packages(),
